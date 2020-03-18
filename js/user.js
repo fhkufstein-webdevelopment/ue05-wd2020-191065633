3f9bd9865f5d0fd3ce39ec5038580ab0c330fa15
Lea Exenberger <1910653633@stud.fh-kufstein.ac.at>
2020-03-18 20:01:29 +0100

    Aufgabe2

--- js/user.js
+++ js/user.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
     var userListBody = $('.userList tbody');
 
     //@todo store and somehow update the current number of users
-
+        sessionStorage.setItem("user","meinName");
+        document.getElementById("result").innerHTML=sessionStorage.getItem("user");
 
     $('.needs-validation').submit(function(event) {
 
@@ -26,6 +27,20 @@ $(document).ready(function() {
         //6. maybe do something else... :-)
 
         //your code follows here
+        //1
+        document.getElementById("user").value="meinName";
+
+        //2
+        onclick.apply("meinName","userList");
+
+        //3
+        document.getElementsByTagName("button");
+
+        //4
+        ontimeupdate.apply("user",userListBody);
+
+        //5
+        userListBody.reset();
 
         return false;
     });
@@ -37,6 +52,12 @@ $(document).ready(function() {
         //2. update number of current users
 
         //your code follows here
+
+        //1
+        removeEventListener("user",onclick,userListBody);
+
+        //2
+        ontimeupdate.apply("user",userListBody);
 
 
     });
